b8c7b1e3975790bcbf3defd56d0ed1ecf4905c30
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-09-09 12:48:17 +0530

    Donate page updated

--- src/components/Donate/DonateStyles.jsx
+++ src/components/Donate/DonateStyles.jsx
@@ -4,9 +4,9 @@ export const Box1 = styled.div`
     padding: 30px;
     width: 100%;
     `
-    export const Box2 = styled.div`
-    display: flex;
-    justify-content: space-between;
+export const Box2 = styled.div`
+display: flex;
+justify-content: space-between;
     `
 export const Box3 = styled.div`
 margin: 1rem 0rem;
@@ -28,6 +28,10 @@ width: 4rem;
 height: 3rem;
 margin: 0.5rem 2rem;
 padding:0rem 1rem;
+outline: none;
+&&::-ms-expand {
+	display: none;
+}
 `
 export const Input3 = styled.input`
 border-radius: 2.5rem;
@@ -37,4 +41,23 @@ width: 15rem;
 height: 3rem;
 margin: 0.5rem 2rem;
 padding:0rem 1rem;
+`
+export const RedButtondiv = styled.div`
+display: flex;
+justify-content: center;
+width: 100%;
+margin-top: 1rem;
+`
+export const RedButton = styled.input`
+border-radius: 2.25rem;
+background: #B92727;
+padding: 0.7rem 2rem;
+color: #FFF;
+border: none;
+&:hover{
+    cursor: pointer;
+    background-color: #FFFFFF;
+    color:#000000;
+    border: 2px solid #B92727;
+}
 `
