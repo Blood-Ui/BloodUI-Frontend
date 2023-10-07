da3e10d7cdaba8fd0b44ba76b9b1bb47beaed352
Sreyas M Pillai <96684846+Sreyas62@users.noreply.github.com>
2023-10-07 23:10:22 +0530

    updated

--- src/components/Donate/Donate.jsx
+++ src/components/Donate/Donate.jsx
@@ -67,7 +67,7 @@ function Donate() {
                 />
               </div>
               <div>
-                <label htmlFor="name">District*</label>
+                <label htmlFor="name">District *</label>
                 <br />
                 <Select2
                   name="bgroup"
