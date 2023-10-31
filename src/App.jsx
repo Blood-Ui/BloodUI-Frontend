9a543d3ecf92be2c7af55bcf2e2c7d53e1750c7b
akashmn <akash003nandan@gmail.com>
2023-10-31 23:37:56 +0530

    landing-section-ongoing

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import SideBar from './components/SideBar/SideBar'
 import Donate from './components/Donate/Donate'
 import Donor from './components/Donor/Donor'
 import Login from './components/Login/Login'
+import LandingPage from './components/LandingPage/LandingPage'
 
 const App = () => {
   return (
     <>
     <div style={{display:'flex'}}>
       {/* <SideBar/>
-      <Donor/> */}
-      <Login />
+      <Donor/>
+      <Login /> */}
+      <LandingPage/>
       </div>
     </>
   )
