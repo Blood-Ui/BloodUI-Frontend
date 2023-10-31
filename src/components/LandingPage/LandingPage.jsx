97cdecb45a01de2e362a949a4b3d125d4875c0f7
akashmn <akash003nandan@gmail.com>
2023-11-01 02:14:40 +0530

    landingpage-text-added

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Container, BoldText, Image, Text1, Text2 } from './LandingPageStyled.jsx'
+import superhero from '../../assets/superhero.png'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <Container>
+      <BoldText>
+        <Text1>Be a <br />SUPERHERO.</Text1>
+        <Text2>Join Us and be the reason for someone's existence.</Text2>
+      </BoldText>
+      <Image src={superhero}></Image>
+    </Container>
   )
 }
 
