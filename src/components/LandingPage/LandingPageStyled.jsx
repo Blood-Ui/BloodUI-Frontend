b145f1fed53e225d41e43d5b3d5beb0f26f80661
akashmn <akash003nandan@gmail.com>
2023-11-19 18:19:46 +0530

    landingPage

--- src/components/LandingPage/LandingPageStyled.jsx
+++ src/components/LandingPage/LandingPageStyled.jsx
@@ -13,19 +13,31 @@ export const BoldText = styled.div`
     display: flex;
     flex-direction: column;
     flex: 7; 
+    width: 50px;
+    height: 200px;
+    border-style: solid;
+
+    /* margin-left: 1300px;
+    margin-top: 1300px; */
     margin-left: 10rem; 
     margin-bottom: 10rem;
+    z-index: 1;
+/*  */
 `
 
 export const Text1 = styled.div`
-    font-weight: 800;
-    font-size: 75px;
+    /* font-weight: 800;
+    font-size: 75px; */
     text-decoration: underline;
+    font-weight: 700;
+    font-size: 45px;
+
 `
 
 export const Text2 = styled.div`
     font-weight: 300;
-    font-size: 23px;
+    /* font-size: 23px; */
+    font-size: 13px;
 `
 
 export const Image = styled.img`
@@ -35,4 +47,5 @@ export const Image = styled.img`
     margin-bottom: 15rem;
     width: 450px;
     height: 342px;
+    z-index: -1;
 `
