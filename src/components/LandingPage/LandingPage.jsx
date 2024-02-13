7a6f248cda107fd4ea66709aa8ac69b69ef5fff0
akashmn <akash003nandan@gmail.com>
2024-02-13 23:53:53 +0530

    test commit

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, MainContent, Heading, Content} from './LandingPageStyled.jsx'
-import superhero from '../../assets/superhero.png'
+import superhero from '../../assets/Superhero.png' 
 import headingImage from '../../assets/headingImage.png'
 
 const LandingPage = () => {
