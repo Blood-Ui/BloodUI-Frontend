5d07b9ec6a03d6f27af028d26712e3e021429441
akashmn <akash003nandan@gmail.com>
2024-02-27 01:31:20 +0530

    landing page structure set up

---
+++ src/components/LandingPage/LandingPageStyled.jsx
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: flex;
+`
+
+export const Content = styled.div`
+    width: 70%;
+    height: 100vh;
+    display: flex;
+`
+export const Logo = styled.div`
+    display: flex;
+    width: 40px;
+`
+export const About = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    padding: 2rem;
+`
+export const ImageDisplay = styled.div`
+    display: flex;
+`
+export const SuperHero = styled.img`
+    width: 100%;
+    height: 100%;
+`
+export const Text1 = styled.div`
+    color: black;
+`
+export const Text2 = styled.div`
+    color: black;
+`
+
