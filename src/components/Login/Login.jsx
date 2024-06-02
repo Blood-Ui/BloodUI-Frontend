b0ff3711aa51822d5849566df516589ca060a53f
Akash M Nandan <akash003nandan@gmail.com>
2024-06-02 18:38:15 +0530

    login page toggle set

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,190 +1,100 @@
-import {useState} from 'react'
-import './LoginStyles.css'
+import { useState } from 'react';
+import './LoginStyles.css';
+import 'boxicons/css/boxicons.min.css';
 
-
-const Login = () => {
-
+const SignInSignUp = () => {
   const [isSignInActive, setIsSignInActive] = useState(true);
 
-  const toggleForm = () => {
-    setIsSignInActive(!isSignInActive);
+  const handleSignInClick = () => {
+    setIsSignInActive(true);
   };
-  
+
+  const handleSignUpClick = () => {
+    setIsSignInActive(false);
+  };
+
   return (
     <div className="container">
-        <div className={`form ${isSignInActive ? 'sign-in-form' : 'sign-up-form active'}`}>
-            <div className="wrapper">
-                <form action="">
-                    <h1>S{}</h1>
-                    <span>or use your account</span>
-                    <div className="input-field">
-                      <input type="email" placeholder='Email' />
-                    </div>
-                    <div className="input-field">
-                      <input type="password" placeholder='Password' />
-                    </div>
-                    <a className="frgt" href="#" >Forgot  your  Password ?</a>
-                    <div className="btn">
-                        <button type="submit">SIGN IN</button>
-                    </div>
-                </form>
+      <div className={`form sign-in-form ${isSignInActive ? 'active' : ''}`}>
+        <div className="wrapper">
+          <form action="">
+            <h1>Sign In</h1>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="bx bxl-facebook"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="bx bxl-linkedin"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="bx bxl-google"></i>
+              </a>
             </div>
+            <span>or use your account</span>
+            <div className="input-field">
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="input-field">
+              <input type="password" placeholder="Password" />
+            </div>
+            <a className="frgt" href="#">
+              Forgot your Password?
+            </a>
+            <div className="btn">
+              <button type="submit">SIGN IN</button>
+            </div>
+          </form>
         </div>
-        <div className="form sign-up-form active">
-          <div className="wrapper">
-            <form action="">
-              <h1>Create Account</h1>
-              <span>or use email for registration</span>
-              <div className="input-feild">
-                <input type="name" placeholder='Name'/>
-              </div>
-              <div className="input-feild">
-                <input type="email" placeholder='Email'/>
-              </div>
-              <div className="input-feild">
-                <input type="password" placeholder='Password'/>
-              </div>
-              <div className="btn">
-                <button type='submit'>SIGN UP</button>
-              </div>
-            </form>
-          </div>
-        </div>
-        <div className="overlay-container">
-          <div className="overlay">
-            <div className="overlay-left">
-              <h1>Welcome Back</h1>
-              <p>To keep connected with us please login with your personal info.</p>
-              <button id='signInButton'>SIGN IN</button>
-            </div>
-            <div className="overlay-right">
-              <h1>Hello Friend!</h1>
-              <p>Enter you personal details and start journey with us</p>
-              <button id='signInButton'>SIGN UP</button>
-            </div>
+      </div>
 
+      <div className={`form sign-up-form ${!isSignInActive ? 'active' : ''}`}>
+        <div className="wrapper">
+          <form action="">
+            <h1>Create Account</h1>
+            <div className="social-media">
+              <a href="#" className="social-icon">
+                <i className="bx bxl-facebook"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="bx bxl-linkedin"></i>
+              </a>
+              <a href="#" className="social-icon">
+                <i className="bx bxl-google"></i>
+              </a>
+            </div>
+            <span>or use your email for registration</span>
+            <div className="input-field">
+              <input type="text" placeholder="Name" />
+            </div>
+            <div className="input-field">
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="input-field">
+              <input type="password" placeholder="Password" />
+            </div>
+            <div className="btn">
+              <button type="submit">SIGN UP</button>
+            </div>
+          </form>
+        </div>
+      </div>
+
+      <div className="overlay-container" style={{ transform: isSignInActive ? 'translateX(100%)' : 'translateX(0)' }}>
+        <div className="overlay" style={{ transform: isSignInActive ? 'translateX(-50%)' : 'translateX(0)' }}>
+          <div className="overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>To keep connected with us please login with your personal info.</p>
+            <button id="signInButton" onClick={handleSignInClick}>SIGN IN</button>
+          </div>
+          <div className="overlay-right">
+            <h1>Hello Friend!</h1>
+            <p>Enter your personal details and start your journey with us</p>
+            <button id="signUpButton" onClick={handleSignUpClick}>SIGN UP</button>
           </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
-
-
-
-// const signInButton = document.querySelector('#signInButton');
-// const signUpButton = document.querySelector('#signUpButton');
-// const overlayContainer = document.querySelector('.container .overlay-container');
-// const overlay = document.querySelector('.container .overlay-container .overlay');
-// const signInform = document.querySelector('.container .sign-in-form');
-// const signUpform = document.querySelector('.container .sign-up-form');
-
-// signInButton.addEventListener('click', () => {
-//     overlayContainer.style.transform = 'translateX(100%)';
-//     overlay.style.transform = 'translateX(-50%)';
-//     signInform.classList.add('active');
-//     signUpform.classList.remove('active');
-// });
-// signUpButton.addEventListener('click', () => {
-//     overlayContainer.style.transform = 'translateX(0)';
-//     overlay.style.transform = 'translateX(0)';
-//     signUpform.classList.add('active');
-//     signInform.classList.remove('active');
-// })
-
-
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <link rel="stylesheet" href="style.css">
-//     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-//     <title>SignIn SignOut Page</title>
-// </head>
-// <body>
-//         <div class="container">
-//         <div class="form sign-in-form">
-//             <div class="wrapper">
-//                 <form action="">
-//                     <h1>Sign In</h1>
-//                     <div class="social-media">
-//                         <a href="#" class="social-icon">
-//                             <i class='bx bxl-facebook'></i>
-//                         </a>
-//                         <a href="#" class="social-icon">
-//                             <i class='bx bxl-linkedin'></i>
-//                         </a>
-//                         <a href="#" class="social-icon">
-//                             <i class='bx bxl-google' ></i>
-//                         </a>
-//                     </div>
-//                     <span>or use your account</span>
-//                     <div class="input-field">
-//                         <input type="email" placeholder="Email">
-//                     </div>
-//                     <div class="input-field">
-//                         <input type="password" placeholder="Password">
-//                     </div>
-//                     <a class="frgt" href="#" >Forgot  your  Password ?</a>
-//                     <div class="btn">
-//                         <button type="submit">SIGN IN</button>
-//                     </div>
-//                 </form>
-//             </div>
-//             </div>
-        
-//         <div class="form sign-up-form active">
-//             <div class="wrapper">
-//                 <form action="">
-//                     <h1>Create Account</h1>
-//                     <div class="social-media">
-//                         <a href="#" class="social-icon">
-//                             <i class='bx bxl-facebook'></i>
-//                         </a>
-//                         <a href="#" class="social-icon">
-//                             <i class='bx bxl-linkedin'></i>
-//                         </a>
-//                         <a href="#" class="social-icon">
-//                             <i class='bx bxl-google' ></i>
-//                         </a>
-//                     </div>
-//                     <span>or use your email for registration</span>
-//                     <div class="input-field">
-//                         <input type="name" placeholder="Name">
-//                     </div>
-//                     <div class="input-field">
-//                         <input type="email" placeholder="Email">
-//                     </div>
-//                     <div class="input-field">
-//                         <input type="password" placeholder="Password">
-//                     </div>
-//                     <div class="btn">
-//                         <button type="submit">SIGN UP</button>
-//                     </div>
-//                 </form>
-//             </div>
-            
-//         </div>
-
-//         <div class="overlay-container">
-//             <div class="overlay">
-//                 <div class="overlay-left">
-//                     <h1>Welcome Back!</h1>
-//                     <p>To keep connected with us please login with your personal info.</p>
-//                     <button id="signInButton">SIGN IN</button>
-//                 </div>
-//                 <div class="overlay-right">
-//                     <h1>Hello Friend!</h1>
-//                     <p>Enter you personal details and start journey with us</p>
-//                     <button id="signUpButton">SIGN UP</button>
-//                 </div>
-//             </div>
-//         </div>
-//     </div>
-// <script src="main.js"></script>
-// </body>
-// </html>
-
+export default SignInSignUp;
