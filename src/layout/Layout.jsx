5aa8edb712c9794e7af5a5aa0459fc9cc779cb00
Akash M Nandan <akash003nandan@gmail.com>
2024-06-20 21:26:53 +0530

    landing page renamed to About

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,7 +3,7 @@ import Donor from "../components/Donor"
 import Donate from "../components/Donate"
 import Login  from "../components/Login"
 import { Box1 } from "../Styles/LayoutStyles"
-import LandingPage from "../components/LandingPage"
+import LandingPage from "../components/About"
 function Layout() {
   return (
     <>
